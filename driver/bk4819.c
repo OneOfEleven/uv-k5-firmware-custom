f9dadfe16936d748b1d3ffe3d71e5767995fd797
bricky149 <shane.bundy@outlook.com>
2023-10-27 14:17:22 +0100

    AGC can attenuate signals that overload the radio

--- driver/bk4819.c
+++ driver/bk4819.c
@@ -346,9 +346,6 @@ void BK4819_DisableAGC(void)
 
 void BK4819_EnableAGC(void)
 {
-	// TODO: See if this attenuates overloading
-	// signals as well as boosting weak ones
-	//
 	// REG_7E
 	//
 	// <15> 0 AGC Fix Mode.
@@ -362,25 +359,29 @@ void BK4819_EnableAGC(void)
 	//
 	// <2:0> 0b110 DC Filter Band Width for Rx (IF In).
 	// 000=Bypass DC filter;
-
+	//
 	// TBR: I believe the above filter values do the following:
 	// High-Pass (300Hz?), Low-Pass (3kHz?), De-Emphasis (attenuates higher freq range)
 	// This assumes the same filter layout as REG_2B
-
 	BK4819_WriteRegister(0x7E,
 		(0u << 15) |      // 0  AGC fix mode
 		(3u << 12) |      // 3  AGC fix index
 		(5u <<  3) |      // 5  DC Filter band width for Tx (MIC In)
 		(6u <<  0));      // 6  DC Filter band width for Rx (I.F In)
 
+	// TBR: fagci has this listed as two values, agc_rssi and lna_peak_rssi
+	// This is why AGC appeared to do nothing as-is for Rx
+	//
 	// REG_62
 	//
-	// TBR: fagci has this listed as two values, agc_rssi and lna_peak_rssi
-	// Could this be why AGC appeared to do nothing? Default: 0xFF (255)
-	BK4819_WriteRegister(0x62, 0x8F);
+	// <15:8> 0xFF AGC RSSI
+	//
+	// <7:0> 0xFF LNA Peak RSSI
+	//
+	// TBR: Using S9+30 (173) and S9 (143) as suggested values
+	BK4819_WriteRegister(0x62, (173u << 8) | (143u << 0));
 
-	// In theory, AGC should auto-adjust LNA values
-	// In practice, it seems to work against us
+	// AGC auto-adjusts the following LNA values, no need to set them ourselves
 	//BK4819_WriteRegister(0x13, (3u << 8) | (5u << 5) | (3u << 3) | (6u << 0));  // 000000 11 101 11 110
 	//BK4819_WriteRegister(0x12, 0x037B);  // 000000 11 011 11 011
 	//BK4819_WriteRegister(0x11, 0x027B);  // 000000 10 011 11 011
